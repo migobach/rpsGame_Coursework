ee0588f0569bf627e5615732441e9e1735b93099
Michael Bach <migobach@gmail.com>
2018-05-14 19:47:06 -0600

    made win statement

--- js/main.js
+++ js/main.js
@@ -3,26 +3,34 @@ var rps = ['rock', 'paper', 'scissors']
 var cresults = document.getElementById('comresult')
 var presults = document.getElementById('perresults')
 var win = document.getElementById('winner')
+var cNum
+var opt 
 
 var rockButton = document.getElementById('rockbtn')
 rockbtn.addEventListener('click', function() {
+  opt = 0
   startGame(0)
   userOutput('rock')
   console.log("Rock")
+  winner(cNum, opt);
 })
 
 var paperButton = document.getElementById('paperbtn')
 paperbtn.addEventListener('click', function() {
+  opt = 1
   startGame(1)
   userOutput('paper')
   console.log("Paper")
+  winner(cNum, opt)
 })
 
 var scissorsButton = document.getElementById('scissorsbtn')
 scissorsbtn.addEventListener('click', function() {
+  opt = 2
   startGame(2)
   userOutput('scissors')
   console.log("Scissors")
+  winner(cNum, opt)
 })
 
 function startGame(opt) {
@@ -35,27 +43,51 @@ function startGame(opt) {
   } else {
     cresults.innerHTML = "The computer chose scissors"
   }
-  winner(cNum, opt)
 }
+
 function winner(cNum, opt) {
-  // debugger
-    if (rps[opt] === rps[cNum]) {
-      win.innerHTML = "It's a tie! Try again."
-    } else if (rps[opt] < rps[cNum]){
-      win.innerHTML = "You lose, try again."
-    } else if (rps[opt] > rps[cNum]){
-      win.innerHTML = "You win!"
-    } else {
-      win.innerHTML = "Not sure"
+  switch(opt) {
+    case 0: 
+      if (cNum === 0) { win.innerHTML = "It's a tie!"}
+      else if (cNum === 1) { win.innerHTML = "You lose, try again."}
+      else { win.innerHTML = "You win!"}
+      break
+    case 1: 
+      if (cNum === 0) { win.innerHTML = "You win! Great job!"}
+      else if (cNum === 1) { win.innerHTML = "It's a tie! Try your luck again."}
+      else { win.innerHTIML = "Sorry, you lose, try again."}
+      break
+    case 2:
+      if (cNum === 0) { win.innerHTML = "Loser!"}
+      else if (cNum === 1) { win.innerHTML = "You a winner! Congrats!"}
+      else { win.innerHTIML = "It's a tie! Try again."} 
+      break
     }
   }
 
+  function userOutput(choice) {
+    presults.innerHTML = ("You chose " + choice)
+  }  
+      
 
+
+
+
+  //   if (rps[opt] === rps[cNum]) {
+  //     win.innerHTML = "It's a tie! Try again."
+  //   } else if (rps[opt] === 0 ||rps[opt] === 1) < (rps[cNum] === 1 ||rps[cNum] === 2){
+  //     win.innerHTML = "You lose, try again."
+  //   } else if (rps[opt] === 0 || 1) > rps[cNum] === 2){
+  //     win.innerHTML = "You win!"
+  //   } else {
+  //     win.innerHTML = "Not sure"
+  //   }
+  // }
+
+ 
     // var (if else for winner)
 
-function userOutput(choice) {
-  presults.innerHTML = ("You chose " + choice)
-}
+
 
 /* 
 paper beats rock
